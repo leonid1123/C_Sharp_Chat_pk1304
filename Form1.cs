cb45c5b15d06adb8f4ff9097fc5abf2ed32b4cae
Leo <47742726+leonid1123@users.noreply.github.com>
2021-10-26 10:36:36 +0300

    Initial commit

---
+++ Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pk1304new
+{
+    public partial class Form1 : Form
+    {
+        String login = "";
+        String pass = "";
+        String loginOK = "MegaNagibator3000";
+        String passOK = "qwerty";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+            //это игнорим
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {//для входа
+            login = textBox1.Text;
+            pass = textBox2.Text;
+
+            //label4.Text = "login:" + login + " pass:" + pass;
+            if (login==loginOK && pass==passOK)
+            {
+                label4.Text = "всё нормально";
+            } else
+            {
+                label4.Text = "всё НЕ нормально";
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {//для регистрации переход в другую форму
+            Form2 frm = new Form2();
+            frm.Show();
+            this.Hide();
+        }
+    }
+}
---
+++ Form2.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pk1304new
+{
+    public partial class Form2 : Form
+    {
+        String newLogin = "";
+        String newPass = "";
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text.Trim() !="")
+            {
+                if (textBox2.Text.Trim() == textBox3.Text.Trim())
+                {
+                    newLogin = textBox1.Text;
+                    newPass = textBox2.Text;
+                    label1.Text = "регистрация успешна";
+                } else
+                {
+                    label1.Text = "";
+                }
+            }
+        }
+    }
+}
